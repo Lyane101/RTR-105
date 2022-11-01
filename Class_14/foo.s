3d7d0c2d0f6f40e7662add3703910cbbdd6ca0b0
Lyane101 <ohwell85@gmail.com>
2022-11-01 14:01:57 +0200

    Class_14

---
+++ Class_14/foo.s
@@ -0,0 +1,35 @@
+	.file	"foo.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"foo(%d, \"%s\");\n"
+	.text
+	.globl	foo
+	.type	foo, @function
+foo:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	stderr(%rip), %rax
+	movq	-16(%rbp), %rcx
+	movl	-4(%rbp), %edx
+	leaq	.LC0(%rip), %rsi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	fprintf@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	foo, .-foo
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
